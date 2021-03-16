2ec3a5ef3875833d0a2d0362575db46ae0cd0fb6
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-03-16 20:32:58 +0530

    Add files via upload

---
+++ kwiiter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyClr2jkqoX74dVcqsuKolEbQd9FTVK4tX4",
+    authDomain: "game-id-emer-and-rub.firebaseapp.com",
+    databaseURL: "https://game-id-emer-and-rub.firebaseio.com",
+    projectId: "game-id-emer-and-rub",
+    storageBucket: "game-id-emer-and-rub.appspot.com",
+    messagingSenderId: "788135384134",
+    appId: "1:788135384134:web:343c935b9c73f0bb51a647"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
